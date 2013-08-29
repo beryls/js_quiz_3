49496b631d560754dcc2d9f8f9d7142201a8f3b6
Beryl <beryl.schragger@gmail.com>
2013-08-29 09:53:58 -0400

    Committing quiz.

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -17,3 +17,64 @@ describe('a quiz', function() {
   });
 
 });
+
+describe('a function called bigDiff', function() {
+
+  it('should return the difference between the largest and second largest numbers in an array', function() {
+    first_array = [3, 6, 9, 12];
+    expect(bigDiff(first_array)).toEqual(3);
+  });
+
+  it('should return that difference even if the array is not in increasing order', function() {
+    second_array = [9, 20, 11, 12];
+    expect(bigDiff(second_array)).toEqual(8);
+  });
+
+});
+
+describe('a function called averageMedian', function() {
+
+  it('should return the difference between the largest and second largest numbers in an array', function() {
+    first_array = [2, 6, 9, 11];
+    expect(averageMedian(first_array)).toEqual(7.5);
+  });
+
+  it('should return that difference even if the array is not in increasing order', function() {
+    second_array = [9, 8, 2, 4, 7];
+    expect(averageMedian(second_array)).toEqual(4.67);
+  });
+
+});
+
+describe('a function called canBalance', function() {
+
+  it('should return true if an array can be split into two parts of equal value', function() {
+    first_array = [1, 2, 1, 1, 1];
+    expect(canBalance(first_array)).toEqual(true);
+  });
+
+  it('should return false if an array cannot be split into two parts of equal value', function() {
+    second_array = [2, 1, 1, 2, 1];
+    expect(canBalance(second_array)).toEqual(false);
+  });
+
+  it('should return true even if the number of elements in each part is heavily unbalanced', function() {
+    third_array = [3, 4, 1, 2, 3, 1];
+    expect(canBalance(third_array)).toEqual(true);
+  });
+
+});
+
+describe('a function called countClumps', function() {
+
+  it('should return the number of times 2 or more of the same number appear sequentially', function() {
+    first_array = [2, 1, 1, 1, 1, 3, 3, 3, 1];
+    expect(countClumps(first_array)).toEqual(2);
+  });
+
+  it('should return the correct count even if there is a break between the clumps', function() {
+    second_array = [9, 3, 3, 4, 4, 4, 6, 7, 7, 7];
+    expect(countClumps(second_array)).toEqual(3);
+  });
+
+});
--- src/quiz.js
+++ src/quiz.js
@@ -13,12 +13,36 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [3, 6, 9, 12] => 3
 //  [9, 20, 11, 12] => 8
 
+var bigDiff = function(array) {
+  var biggest = 0;
+  var second = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] >= biggest) {
+      second = biggest;
+      biggest = array[i];
+    } else if (array[i] > second) {
+      second = array[i];
+    }
+  }
+  return biggest - second;
+}
+
 // return the average of the 2 or 3 numbers in the middle of the array
 // (2 if the array has an even number of elements)
 // (3 if the array has an odd number of elements).
 // averageMedian(array)
 //  [2, 6, 9, 11] => 7.5
 //  [9, 8, 2, 4, 7] => 4.67
+
+var averageMedian = function(array) {
+  var size = array.length;
+  if (size % 2 === 0) {
+    return parseFloat(((array[size/2 - 1] + array[size/2]) / 2).toFixed(2));
+  } else {
+    var middle = (size - 1) / 2;
+    return parseFloat(((array[middle - 1] + array[middle] + array[middle + 1]) / 3).toFixed(2));
+  }
+}
 
 // return true if you can split the array into parts
 // where the sum of one portion equals the sum of the other portion.
@@ -27,7 +51,51 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [2, 1, 1, 2, 1] => false
 //  [3, 4, 1, 2, 3, 1] => true
 
+var canBalance = function(array) {
+  var sum_array = 0
+  var half_sum = 0
+  var balance_half = 0
+  var num_item = 0
+  for (i = 0; i < array.length; i++) {
+    sum_array += array[i];
+  }
+  if (sum_array%2 !== 0) {
+    return false;
+  } else {
+    half_sum = sum_array / 2
+    while (balance_half < half_sum) {
+      balance_half += array[num_item];
+      num_item += 1;
+    }
+    if (balance_half === half_sum) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
 // Returns the number of times 2 or more of the same number appear sequentially.
 // countClumps(array)
 //  [2, 1, 1, 1, 1, 3, 3, 3, 1] => 2
 //  [9, 3, 3, 4, 4, 4, 6, 7, 7, 7] => 3
+
+var countClumps = function(array) {
+  var clump_sum = 0
+  var last = null
+  var in_clump = false
+  for(i = 0; i < array.length; i++) {
+    if (array[i] === last) {
+      if (in_clump === false) {
+        clump_sum++;
+        in_clump = true;
+      }
+    } else {
+      in_clump = false;
+    }
+    last = array[i];
+  }
+  return clump_sum
+}
+
+
